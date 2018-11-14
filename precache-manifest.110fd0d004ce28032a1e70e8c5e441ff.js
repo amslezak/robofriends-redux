2211e1d8348c146e5b3cb5b056e7117ccdd1d636
Andy Slezak <andy.slezak@gmail.com>
2018-11-13 23:31:08 -0500

    Updates

---
+++ precache-manifest.110fd0d004ce28032a1e70e8c5e441ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2447cb25e80291f6f221",
+    "url": "/robofriends-redux/static/css/main.1ff3b248.chunk.css"
+  },
+  {
+    "revision": "2447cb25e80291f6f221",
+    "url": "/robofriends-redux/static/js/main.2447cb25.chunk.js"
+  },
+  {
+    "revision": "909f42e92cdf6409bbe9",
+    "url": "/robofriends-redux/static/css/1.dcb71130.chunk.css"
+  },
+  {
+    "revision": "909f42e92cdf6409bbe9",
+    "url": "/robofriends-redux/static/js/1.909f42e9.chunk.js"
+  },
+  {
+    "revision": "9e0b58d846368f426a94",
+    "url": "/robofriends-redux/static/js/runtime~main.9e0b58d8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "eaaee48e4971c05b6ef6a1ec0b1b7bbd",
+    "url": "/robofriends-redux/index.html"
+  }
+];
